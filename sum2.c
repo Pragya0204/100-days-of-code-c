f1892c2492acd26d8561810ab2a937d2bb169069
Pragya0204 <Miss.41637@stu.upes.ac.in>
2026-09-01 22:49:59 +0530

    Create sum2.c

---
+++ sum2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+int b;
+int sum;
+printf("Enter first number");
+scanf("%d",&a);
+printf("Enter second number");
+scanf("%d",&b);
+sum=a+b;
+printf("Sum of numbers %d",sum);
+return 0;
+}
