cc2e93871b0409df646b02a835280c167472eedd
Pragya0204 <Miss.41637@stu.upes.ac.in>
2026-09-10 23:14:01 +0530

    q19 and q20

---
+++ Day10/q19.c
@@ -0,0 +1,41 @@
+//Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+
+/*
+Sample Test Cases:
+Input 1:
+3 3 3
+Output 1:
+Equilateral
+
+Input 2:
+3 3 4
+Output 2:
+Isosceles
+
+Input 3:
+2 3 4
+Output 3:
+Scalene
+
+*/
+
+
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter side lenghts");
+scanf("%d%d%d",&a,&b,&c);
+if(a+b>c&&a+c>b&&b+c>a)
+{
+if(a==b&&b==c)
+printf("Equilateral Triangle");
+else if(a==b||a==c||b==c)
+printf("Isosceles Triangle");
+else
+printf("Scalene Triangle");
+}
+else
+printf("Not a traingle");
+return 0;
+}
---
+++ Day10/q20.c
@@ -0,0 +1,48 @@
+//Q20: Write a program to display the day of the week based on a number (1–7) using switch-case.
+
+/*
+Sample Test Cases:
+Input 1:
+1
+Output 1:
+Monday
+
+Input 2:
+5
+Output 2:
+Friday
+
+*/
+
+#include<stdio.h>
+int main()
+{
+int op;
+printf("Enter any number from 1 to 7\n");
+scanf("%d",&op);
+switch(op)
+{
+case 1:
+printf("Sunday");
+break;
+case 2:
+printf("Monday");
+break;
+case 3:
+printf("Tuesday");
+break;
+case 4:
+printf("Wednesday");
+break;
+case 5:
+printf("Thursday");
+break;
+case 6:
+printf("Friday");
+break;
+case 7:
+printf("Saturday");
+break;
+}
+return 0;
+}
