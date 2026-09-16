b8f8336b2a3b1790f85af10defbc2756af034cd8
Pragya0204 <Miss.41637@stu.upes.ac.in>
2026-09-16 20:15:32 +0530

    q29 and q30

---
+++ Day15/q29.c
@@ -0,0 +1,29 @@
+//Q29: Write a program to calculate the factorial of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+120
+
+Input 2:
+3
+Output 2:
+6
+
+*/
+
+#include<stdio.h>
+int main()
+{
+int n,f=1;
+printf("Enter a number");
+scanf("%d",&n);
+for(int i=n;i>=1;i--)
+{
+f=f*i;
+}
+printf("factorial is %d",f);
+return 0;
+}
---
+++ Day15/q30.c
@@ -0,0 +1,31 @@
+//Q30: Write a program to reverse a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4321
+
+Input 2:
+100
+Output 2:
+1
+
+*/
+
+#include<stdio.h>
+int main()
+{
+int n,r,a=0;
+printf("Enter a number");
+scanf("%d",&n);
+while(n!=0)
+{
+r=n%10;
+a=a*10+r;
+n=n/10;
+}
+printf("%d",a);
+return 0;
+}
