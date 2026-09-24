d8e28014f95cee262f9aee19aee3b2b3c2f30eb1
Pragya0204 <Miss.41637@stu.upes.ac.in>
2026-09-25 02:39:26 +0530

    q34 nand q35

---
+++ Day20/q41.c
@@ -0,0 +1,38 @@
+//Q41: Write a program to swap the first and last digit of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4231
+
+Input 2:
+1001
+Output 2:
+1001
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, first, last, temp, power = 1;
+
+    scanf("%d", &n);
+
+    last = n % 10;
+    temp = n;
+
+    while (temp >= 10) {
+        temp /= 10;
+        power *= 10;
+    }
+    first = temp;
+
+    int swapped = (last * power) + (n % power - last) + first;
+
+    printf("%d\n", swapped);
+
+    return 0;
+}
